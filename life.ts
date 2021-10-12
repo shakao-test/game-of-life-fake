bec06144013ed9e0b5651fb277687301b74a8c07
shakao-test <61802990+shakao-test@users.noreply.github.com>
2021-10-12 14:25:54 -0700

    Update pxt.json, life.ts

---
+++ life.ts
@@ -0,0 +1,13 @@
+// Add your code here
+//% color="#AA278D" weight=100
+namespace life {
+    //% block="is alive $alive at x $x y $y"
+    export function getIsAlive(alive: boolean, x: number, y: number) {
+        return true;
+    }
+
+    //% block="set is alive $alive at x $x y $y"
+    export function setIsAlive(alive: boolean, x: number, y: number) {
+
+    }
+}
